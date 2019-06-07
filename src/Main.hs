5a5681bbee02aa3fa8262f1714a20f6846bab53f
thom.koehler <thom.koehler@gmail.com>
2019-06-07 21:10:42 +0200

    add VinylTest

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import qualified RunQ
 import qualified DepTypes
 import qualified Fun
+import qualified VinylTest
 
 main :: IO ()
-main = RunQ.printInstance2
+main = VinylTest.test
