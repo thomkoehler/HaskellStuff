f6e8f620c248036e88fe179d839c9f7ebc894613
thom.koehler <thom.koehler@gmail.com>
2019-06-21 18:46:20 +0200

    remove SIngletonTest

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,6 @@ import qualified RunQ
 import qualified DepTypes
 import qualified Fun
 import qualified VinylTest
-import qualified SingletonTest
 
 main :: IO ()
-main = SingletonTest.test
+main = VinylTest.test
