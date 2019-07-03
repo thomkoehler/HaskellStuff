72e0a58788a4b2a3a3cf0d680e7b7fbd10404536
Koehler <thomas.koehler@realtech.com>
2019-07-03 15:34:37 +0200

    add toConfig

--- src/ConfReader.hs
+++ src/ConfReader.hs
@@ -1,50 +1,58 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module ConfReader where
 
-import GHC.Generics as G
-import Control.Lens
-import Data.Aeson
-import Data.Binary.Builder
-
-
-data ConfigType 
-    = Type0 
-    | Type1 
-    | Type2 
-    deriving(Show, Generic)
+import           Control.Lens
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Binary.Builder
+import           Data.HashMap.Strict
+import           Data.Text
+import qualified Data.Text           as T
+import           GHC.Generics        as G
 
 data Config = Config
-    {
-        _confType :: ConfigType,
-        _entry1 :: Int,
-        _entry2 :: String
+    { 
+      intEntry :: Int,
+      stringEntry :: String,
+      boolEntry :: Bool
     }
-    deriving(Show, Generic)
-
-makeLenses ''Config
-
-instance ToJSON ConfigType where
-    toEncoding = genericToEncoding defaultOptions
+  deriving (Show, Generic)
 
 instance ToJSON Config where
-    toEncoding = genericToEncoding defaultOptions
-
+  toEncoding = genericToEncoding defaultOptions
 
 printValue :: Encoding -> String
-printValue value = 
-    let 
-        builder = fromEncoding value
-    in
-        show $ toLazyByteString builder 
-
+printValue value =
+  let builder = fromEncoding value
+   in show $ toLazyByteString builder
 
 test :: IO ()
 test = do
-    let 
-        conf = Config Type0 1 "Hello"
-        repConf = G.from conf
-        jsonConf = genericToEncoding defaultOptions conf
-    
-    print $ printValue jsonConf
+  let conf = Config 1 "Hello" False
+      repConf = G.from conf
+      jsonConf = genericToJSON defaultOptions conf
+      genBool = G.from True
+  print $ toConfig conf
+
+toConfig config =
+  case genericToJSON defaultOptions config of
+    (Object valueMap) ->
+      let values = toList valueMap
+          lines = Prelude.map valueToConfEntry values
+       in T.unlines lines
+    _ -> error "Only objects supported"
+
+valueToText :: Value -> T.Text
+valueToText (String text)   = text
+valueToText (Number number) = T.pack $ show number
+valueToText (Bool bool)     = T.pack $ show bool
+valueToText (Object _)      = error "Object value is not supported"
+valueToText (Array _)       = error "Array value is not supported"
+valueToText Null            = error "Null value is not supported"
+
+valueToConfEntry :: (T.Text, Value) -> T.Text
+valueToConfEntry (name, value) = T.concat [name, " = ", valueToText value]
